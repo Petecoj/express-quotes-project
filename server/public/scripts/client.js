a134ae0f9d5e8a4d7ea4ab4ec6e9c08c50414c20
petecoj <petecojohns@gmail.com>
2018-06-28 17:09:41 -0500

    sorry i forgot the git ignore so i added that...i also finished the basemode goals of adding inputs and a submit button to update quotes list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,10 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('jquery is loaded');
     loadQuotesList();
+    $('#submit').on('click',submitQuote);
+    
 }
+
 
 function loadQuotesList() {
     $.ajax({
@@ -13,6 +16,7 @@ function loadQuotesList() {
         type: 'GET'
     })
     .done(function (response) {
+        $('#quotesList').empty();
         for (i = 0; i < response.length; i++) {
             $('#quotesList').append(`<li>"${response[i].quote}"</li>`)
             $('#quotesList').append(`<br><li class = "author">${response[i].author}</li><br>`)
@@ -21,4 +25,25 @@ function loadQuotesList() {
     .fail(function(errorResponse){
         alert('request failed error' + errorResponse.status)
     })
+}
+function submitQuote(){
+    let quote = $('#quote').val();
+    let author = $('#author').val();
+    console.log(quote,author);
+
+    $.ajax({
+        url: '/famousQuotes',
+        method: 'POST',
+        data: {
+            quote: quote,
+            author: author
+        }
+    })
+    .done(function (response) {
+        
+        loadQuotesList();
+    })
+   
+   
+    
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
 let quotes = [{
         quote: "Don't cry because it's over, smile because it happened.",
@@ -26,11 +33,19 @@ let quotes = [{
         author: "Mahatma Gandhi"
     }
 ]
-app.use(express.static('server/public'));
-app.get('/famousquotes', function (req, res) {
-    res.send(quotes);
-})
+
+
 
 app.listen(port, function () {
     console.log('listening on port', port);
 });
+app.get('/famousquotes', function (req, res) {
+    res.send(quotes);
+})
+app.post('/famousquotes', function(req, res){
+    console.log('here is a request', req.body);
+    
+    quotes.push(req.body);
+    res.sendStatus(201)
+})
+
