648a6ca942563e5d01f6278011af6c5a5240842c
petecoj <petecojohns@gmail.com>
2018-06-28 08:48:03 -0500

    final changes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,23 @@ console.log('working')
 
 $(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('jquery is loaded');
-    
+    loadQuotesList();
+}
+
+function loadQuotesList() {
+    $.ajax({
+        url: '/famousQuotes',
+        type: 'GET'
+    })
+    .done(function (response) {
+        for (i = 0; i < response.length; i++) {
+            $('#quotesList').append(`<li>"${response[i].quote}"</li>`)
+            $('#quotesList').append(`<br><li class = "author">${response[i].author}</li><br>`)
+        }
+    })
+    .fail(function(errorResponse){
+        alert('request failed error' + errorResponse.status)
+    })
 }
--- server/server.js
+++ server/server.js
@@ -4,9 +4,33 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+let quotes = [{
+        quote: "Don't cry because it's over, smile because it happened.",
+        author: "Dr. Suess"
+    },
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde"
 
+    },
+    {
+        quote: "So many books, so little time.",
+        author: "Frank Zappa"
+    },
+    {
+        quote: "you only live once, but if you do it right, once is enough.",
+        author: "Mae West"
+    },
+    {
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandhi"
+    }
+]
 app.use(express.static('server/public'));
+app.get('/famousquotes', function (req, res) {
+    res.send(quotes);
+})
 
 app.listen(port, function () {
     console.log('listening on port', port);
-  });
+});
